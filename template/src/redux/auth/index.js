ad87f76774a7715a92b56eacd383bf8c69f78cf2
myckhel <myckhel1@hotmail.com>
2022-03-01 01:58:57 +0100

    fixed redux token storage

--- template/src/redux/auth/index.js
+++ template/src/redux/auth/index.js
@@ -1,39 +1,34 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const INIT_STATE = {
-  token: JSON.parse(localStorage.getItem('token')),
-  authUser: JSON.parse(localStorage.getItem('user')),
+  token: localStorage.getItem("token"),
+  authUser: JSON.parse(localStorage.getItem("user")),
 };
 
-const {actions, reducer} = createSlice({
-  name: 'auth',
+const { actions, reducer } = createSlice({
+  name: "auth",
   initialState: INIT_STATE,
   reducers: {
-    setToken: (state,{payload: token}) => {
-      state.token = token
-      localStorage.setItem("token", JSON.stringify(token));
+    setToken: (state, { payload: token }) => {
+      state.token = token;
+      localStorage.setItem("token", token);
     },
 
     logoutUser: (state) => {
-      state.token = null
-      state.authUser = null
+      state.token = null;
+      state.authUser = null;
       localStorage.removeItem("token");
       localStorage.removeItem("user");
     },
 
     signOutSuccess: () => {},
 
-    storeUser: (state, {payload}) => {
-      state.authUser = payload
-    }
+    storeUser: (state, { payload }) => {
+      state.authUser = payload;
+    },
   },
 });
 
-export const {
-  setToken,
-  logoutUser,
-  signOutSuccess,
-  storeUser,
-} = actions;
+export const { setToken, logoutUser, signOutSuccess, storeUser } = actions;
 
 export default reducer;
